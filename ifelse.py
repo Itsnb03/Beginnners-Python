2dbc594ba378d9d718fdbf0d1e5cad9e5b4b1b22
bnitish101 <bnitish101@gmail.com>
2023-06-30 01:13:16 +0530

    value change

---
+++ ifelse.py
@@ -0,0 +1,10 @@
+age = int(input("Enter your age :"))
+if(age >= 18):
+    print("Yes, you can drive ")
+elif(age == 1):
+    print("You are a kid")
+elif(age == 10):
+    print("You have to be 18+ ")
+else:
+    print("No, you can go home ")
+print("end of program ")
