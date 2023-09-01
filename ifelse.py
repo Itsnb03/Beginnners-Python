2916238f34d8fb3ae490e1ca6942e2dd607024dc
Itsnb03 <132834489+Itsnb03@users.noreply.github.com>
2023-09-01 19:16:55 +0530

    Add files via upload

--- ifelse.py
+++ ifelse.py
@@ -1,10 +1,13 @@
-age = int(input("Enter your age :"))
-if(age >= 18):
-    print("Yes, you can drive ")
-elif(age == 1):
-    print("You are a kid")
-elif(age == 10):
-    print("You have to be 18+ ")
+age = int(input("Enter your age: "))
+
+if(age>=18):
+    print("yes you can drive")
+
+elif(age==10):
+    print("you are a kid")
 else:
-    print("No, you can go home ")
-print("end of program ")
+    print("no, you can go home")
+
+print("End of a program")
+
+
